f2bdd0494f1949b60d5d36048f8d23dc6aeb63f2
zoshhhh <129791585+Zoshhhh@users.noreply.github.com>
2024-05-06 14:18:21 +0200

    v1

---
+++ z_sit/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'sit on object | by zoshhhh'
+
+author 'https://github.com/Zoshhhh'
+
+version '1.0'
+
+client_scripts {
+    'config.lua',
+	'client.lua',
+}
